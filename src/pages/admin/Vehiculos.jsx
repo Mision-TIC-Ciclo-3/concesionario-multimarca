212b06cbb003ca52a2a97453a7ed4e907e5ac99d
Daniel Saldarriaga <dsaldarriaga@prevalentware.com>
2021-09-28 14:03:07 -0500

    agregar tabla y formulario de creacion de vehiculos

--- src/pages/admin/Vehiculos.jsx
+++ src/pages/admin/Vehiculos.jsx
@@ -3,71 +3,131 @@ import React, { useEffect, useState } from 'react';
 // realizar un formulario que le pida al usuario su edad y muestre un mensaje
 // que diga si el usuario es mayor de edad o no
 
+const vehiculosBackend = [
+  {
+    nombre: 'Corolla',
+    marca: 'Toyota',
+    modelo: 2014,
+  },
+  {
+    nombre: 'Sandero',
+    marca: 'Renault',
+    modelo: 2020,
+  },
+  {
+    nombre: 'Rav4',
+    marca: 'Toyota',
+    modelo: 2021,
+  },
+  {
+    nombre: 'Fiesta',
+    marca: 'Ford',
+    modelo: 2017,
+  },
+  {
+    nombre: 'Mazda 3',
+    marca: 'Mazda',
+    modelo: 2020,
+  },
+  {
+    nombre: 'Chevrolet',
+    marca: 'Onix',
+    modelo: 2020,
+  },
+  {
+    nombre: 'Chevrolet',
+    marca: 'Onix',
+    modelo: 2020,
+  },
+];
+
 const Vehiculos = () => {
-  const [edad, setEdad] = useState(0);
-  const [esMenorDeEdad, setEsMenorDeEdad] = useState(false);
-  const [mostrarCamposAdicionales, setMostrarCamposAdicionales] = useState(false);
+  const [mostrarTabla, setMostrarTabla] = useState(true);
+  const [vehiculos, setVehiculos] = useState([]);
+  const [textoBoton, setTextoBoton] = useState('Crear Nuevo Vehículo');
 
   useEffect(() => {
-    if (edad >= 18) {
-      setEsMenorDeEdad(false);
-    } else {
-      setEsMenorDeEdad(true);
-    }
-  }, [edad]);
+    //obtener lista de vehículos desde el backend
+    setVehiculos(vehiculosBackend);
+  }, []);
 
+  useEffect(() => {
+    if (mostrarTabla) {
+      setTextoBoton('Crear Nuevo Vehículo');
+    } else {
+      setTextoBoton('Mostrar Todos los vehículos');
+    }
+  }, [mostrarTabla]);
   return (
-    <form className='flex flex-col'>
-      <h2>Formulario de Creación de vehículos</h2>
-      <label htmlFor='edad'>
-        Por favor ingrese su edad
-        <input
-          value={edad}
-          onChange={(e) => {
-            setEdad(e.target.value);
+    <div className='flex h-full w-full flex-col items-center justify-start p-8'>
+      <div className='flex flex-col'>
+        <h2 className='text-3xl font-extrabold text-gray-900'>
+          Página de administración de vehículos
+        </h2>
+        <button
+          onClick={() => {
+            setMostrarTabla(!mostrarTabla);
           }}
-          className='border border-gray-600'
-          name='edad'
-          type='number'
-        />
-      </label>
-      {esMenorDeEdad ? (
-        <span className='text-3xl text-red-500'>
-          ¡Usted es menor de edad, no puede hacer pagos!
-        </span>
+          className='text-white bg-indigo-500 p-5 rounded-full m-6 w-28 self-end'
+        >
+          {textoBoton}
+        </button>
+      </div>
+      {mostrarTabla ? (
+        <TablaVehiculos listaVehiculos={vehiculos} />
       ) : (
-        <span className='text-3xl text-green-500'>
-          ¡Usted es mayor de edad, sí puede hacer pagos!
-        </span>
+        <FormularioCreacionVehiculos />
       )}
-      <button
-        onClick={() => setMostrarCamposAdicionales(!mostrarCamposAdicionales)}
-        type='button'
-        className='text-white bg-indigo-500'
-      >
-        Mostrar campos adicionales
-      </button>
-      {mostrarCamposAdicionales && (
-        <div>
-          <input className='border bg-gray-400 my-2 p-3' placeholder='dato nuevo' type='text' />
-          <input className='border bg-gray-400 my-2 p-3' placeholder='dato nuevo' type='text' />
-          <input className='border bg-gray-400 my-2 p-3' placeholder='dato nuevo' type='text' />
-          <input className='border bg-gray-400 my-2 p-3' placeholder='dato nuevo' type='text' />
-          <input className='border bg-gray-400 my-2 p-3' placeholder='dato nuevo' type='text' />
-        </div>
-      )}
-    </form>
+    </div>
   );
 };
 
-// if (esMenorDeEdad) {
-//   return (
-//     <span className='text-3xl text-red-500'>¡Usted es menor de edad, no puede hacer pagos!</span>
-//   );
-// } else {
-//   return (
-//     <span className='text-3xl text-green-500'>¡Usted es mayor de edad, sí puede hacer pagos!</span>
-//   );
-// }
+const TablaVehiculos = ({ listaVehiculos }) => {
+  useEffect(() => {
+    console.log('este es el listado de vehiculos en el componente de tabla', listaVehiculos);
+  }, [listaVehiculos]);
+  return (
+    <div className='flex flex-col items-center justify-center'>
+      <h2 className='text-2xl font-extrabold text-gray-800'>Todos los vehículos</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>Nombre del vehículo</th>
+            <th>Marca del vehículo</th>
+            <th>Modelo del vehículo</th>
+          </tr>
+        </thead>
+        <tbody>
+          {listaVehiculos.map((vehiculo) => {
+            return (
+              <tr>
+                <td>{vehiculo.nombre}</td>
+                <td>{vehiculo.marca}</td>
+                <td>{vehiculo.modelo}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+const FormularioCreacionVehiculos = () => {
+  return (
+    <div className='flex flex-col items-center justify-center'>
+      <h2 className='text-2xl font-extrabold text-gray-800'>Crear nuevo vehículo</h2>
+      <form className='grid grid-cols-2'>
+        <input className='bg-gray-50 border border-gray-600 p-2 rounded-lg m-2' type='text' />
+        <input className='bg-gray-50 border border-gray-600 p-2 rounded-lg m-2' type='text' />
+        <input className='bg-gray-50 border border-gray-600 p-2 rounded-lg m-2' type='text' />
+        <input className='bg-gray-50 border border-gray-600 p-2 rounded-lg m-2' type='text' />
+        <button className='col-span-2 bg-green-400 p-2 rounded-full shadow-md hover:bg-green-600 text-white'>
+          Guardar vehiculo
+        </button>
+      </form>
+    </div>
+  );
+};
 
 export default Vehiculos;
